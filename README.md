4c4c435eae5ba1a0e2ac9e8093f458fe4fa1c14a
Tim Sutton <tim@kartoza.com.com>
2022-01-31 23:10:52 +0000

    added backups script

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 Orchestration for training.kartoza.com
 
+To run do:
+
+```
+./init-letsencrypt.sh
+docker-compose build db-backup
+docker-compose up -d 
+
+```
+
 Tim Sutton
 
 2021
--- pull-from-server.sh
+++ pull-from-server.sh
@@ -4,4 +4,7 @@ scp training.kartoza.com:/root/docker-compose.yml .
 scp training.kartoza.com:/root/init-letsencrypt.sh .
 rsync -av training.kartoza.com:/root/nginx_conf .
 rsync -av training.kartoza.com:/root/docker_data .
+rsync -av training.kartoza.com:/root/docker-mariadb-backup-master .
+rsync -av training.kartoza.com:/root/backups .
+
 
